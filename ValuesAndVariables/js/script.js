8ce14cc6a68c0a3de7807b90861e31f97f3bebab
brandondukes <brandond.rpg@gmail.com>
2014-01-19 22:06:22 -0500

    Values & Variables exercises

--- ValuesAndVariables/js/script.js
+++ ValuesAndVariables/js/script.js
@@ -1,1 +1,44 @@
-alert("This is in the js file.");
+// JavaScript
+
+/*
+var yearBorn = 1979;
+
+
+console.log("Hello world!");
+console.log(yearBorn);
+*/
+
+/*
+alert(yearBorn);
+*/
+
+
+// Defining Variables
+
+/*
+var age; //declaration
+age = 34; //definition
+//storage container = value
+
+var yearOfBirth = 1979; //declaring and defining on the same line
+*/
+
+
+// Variables of Different Types, Numbers, Boolean and Strings
+
+/*
+var age = 34; // Number variable
+
+var isStudent = false; //Boolean Variable
+
+var firstName = "Brandon"; //String Variable
+*/
+
+
+// Arrays
+
+var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"]; //0,1,2,3
+
+muppetNames[4] = "Fozzy";
+
+console.log(muppetNames[4]);
